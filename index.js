121dac54f12567138c002e39e44bc1d7a97c1cfb
Mohammed Alauddin <75433873+Alauddiin@users.noreply.github.com>
2021-07-23 22:11:40 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let colorChangerBtn = document.querySelector('#color-changer-btn');
+let colorCode = document.querySelector('#color-code');
+let body= document.querySelector('body')
+let randomInt = (max,min)=>Math.floor(Math.random()*(max-min+1)+min);
+let randomColor= ()=>`rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
+
+
+
+colorChangerBtn.addEventListener('click',function(e){
+    body.style.backgroundColor=randomColor();
+    colorCode.innerHTML= randomColor();
+
+})
